4207fb3ee0fc58507f0116972431e83d42c7cb64
alexhenao.001 <alexhenao.001@gmail.com>
2024-01-18 23:03:29 -0500

    Add new classes

---
+++ content-calendar/src/main/java/dev/alexhenao/contentcalendar/controller/ContentController.java
@@ -0,0 +1,12 @@
+package dev.alexhenao.contentcalendar.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/content")
+public class ContentController {
+
+
+
+}
---
+++ content-calendar/src/main/java/dev/alexhenao/contentcalendar/repository/ContentCollectionRepository.java
@@ -0,0 +1,7 @@
+package dev.alexhenao.contentcalendar.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ContentCollectionRepository {
+}
