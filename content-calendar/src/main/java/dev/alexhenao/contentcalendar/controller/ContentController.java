066960c43ae45fe0330b1a0a1065efbd6a82fb22
alexhenao.001 <alexhenao.001@gmail.com>
2024-01-18 23:19:08 -0500

    Add RequestMapping and GetMapping to Controller

--- content-calendar/src/main/java/dev/alexhenao/contentcalendar/controller/ContentController.java
+++ content-calendar/src/main/java/dev/alexhenao/contentcalendar/controller/ContentController.java
@@ -1,12 +1,27 @@
 package dev.alexhenao.contentcalendar.controller;
 
+import dev.alexhenao.contentcalendar.model.Content;
+import dev.alexhenao.contentcalendar.repository.ContentCollectionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/content")
 public class ContentController {
 
+    private final ContentCollectionRepository repository;
 
+    public ContentController(ContentCollectionRepository repository) {
+        this.repository = repository;
+    }
 
+    // make a request and find all the pieces of content in the system
+    @GetMapping("")
+    public List<Content> findAll() {
+        return repository.findAll();
+    }
 }
