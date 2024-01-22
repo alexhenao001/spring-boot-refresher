35fe2d0abd44d523c463cb79937857245b825e12
alexhenao.001 <alexhenao.001@gmail.com>
2024-01-21 20:46:18 -0500

    test Http Methods

--- content-calendar/src/main/java/dev/alexhenao/contentcalendar/controller/ContentController.java
+++ content-calendar/src/main/java/dev/alexhenao/contentcalendar/controller/ContentController.java
@@ -2,10 +2,9 @@ package dev.alexhenao.contentcalendar.controller;
 
 import dev.alexhenao.contentcalendar.model.Content;
 import dev.alexhenao.contentcalendar.repository.ContentCollectionRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -24,4 +23,17 @@ public class ContentController {
     public List<Content> findAll() {
         return repository.findAll();
     }
+
+    @GetMapping("/{id}")
+    public Content findById(@PathVariable Integer id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not Found!"));
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    public void create(@RequestBody Content content) {
+        repository.save(content);
+    }
+
 }
