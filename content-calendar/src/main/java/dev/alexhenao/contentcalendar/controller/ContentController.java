58c4e93f88193a272ff94c16f63f7b7e652a85d9
alexhenao.001 <alexhenao.001@gmail.com>
2024-01-21 21:25:04 -0500

    add delete and update methods to controller and repository class

--- content-calendar/src/main/java/dev/alexhenao/contentcalendar/controller/ContentController.java
+++ content-calendar/src/main/java/dev/alexhenao/contentcalendar/controller/ContentController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/content")
+@CrossOrigin
 public class ContentController {
 
     private final ContentCollectionRepository repository;
@@ -34,6 +35,22 @@ public class ContentController {
     @PostMapping("")
     public void create(@RequestBody Content content) {
         repository.save(content);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    public void update(@RequestBody Content content, @PathVariable Integer id) {
+        if (!repository.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found!");
+        }
+
+        repository.save(content);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id) {
+        repository.delete(id);
     }
 
 }
